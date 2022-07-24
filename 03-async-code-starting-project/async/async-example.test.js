663b0fe953b35893294d2b3de170df3405dc8159
wixarika <agahydzik2@gmail.com>
2022-07-24 15:38:03 +0200

    async commit

---
+++ 03-async-code-starting-project/async/async-example.test.js
@@ -0,0 +1,33 @@
+import {expect, it} from 'vitest';
+import { generateToken, generateTokenPromise } from './async-example';
+
+it('should generate a token value', (done) => {
+    const testUserEmail = 'test@test.com';
+
+    generateToken(testUserEmail, (err, token)=> {
+        try {
+            expect(token).toBeDefined();
+            //expect(token).toBe(2);
+            done();
+        } catch (err) {
+            done(err);
+        }
+    })
+});
+
+    it('should generate a token value', () => {
+        const testUserEmail = 'test@test.com';
+
+        return
+expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined();
+    });
+
+//alternative to above test
+it('should generate a token value', async () => {
+    const testUserEmail = 'test@test.com';
+
+    const token = await generateTokenPromise(testUserEmail);
+
+    expect(token).toBeDefined();
+});
+
